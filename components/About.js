246c19b733a4d96da6f51962e2f1ffbc9f9ae196
Brock Boren <brock.boren@gmail.com>
2019-03-19 16:38:05 -0500

    Create About page skeleton.

--- components/About.js
+++ components/About.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AboutPage, PageHeader, TeamContainer, MemberRow, Member } from './styles/AboutStyles';
+import { AboutPage, PageHeader, TeamContainer } from './styles/AboutStyles';
 
 export class About extends Component {
 	render() {
@@ -17,10 +17,46 @@ export class About extends Component {
 					<h3 className="team-greeting">Come meet the team.</h3>
 				</PageHeader>
 				<TeamContainer>
-					<MemberRow>
-						<Member />
-						<Member />
-					</MemberRow>
+					<div className="member-row" id="top">
+						<div className="member" id="first">
+							<div className="folder">
+								<img src="/static/images/about-folder-member-1.svg" alt="folder background" />
+							</div>
+							<div className="person">
+								<img src="/static/images/about-tessa.jpg" alt="Tessa" />
+							</div>
+							<div className="bio">
+								<img src="/static/images/about-bio-tessa.png" alt="Tessa's bio" />
+							</div>
+						</div>
+						<div className="member" id="second">
+							<div className="folder">
+								<img src="/static/images/about-folder-member-2.svg" alt="folder background" />
+							</div>
+							<div className="person">
+								<img src="/static/images/about-gina.jpg" alt="Gina" />
+							</div>
+							<div className="bio">
+								<img src="/static/images/about-bio-gina.png" alt="Gina's bio" />
+							</div>
+						</div>
+					</div>
+					<div id="grey-bg" />
+					<div id="file-cabinet">
+						<img src="/static/images/file-cabinet.svg" alt="file cabinet" />
+					</div>
+					<div id="monitor">
+						<img src="/static/images/monitor.svg" alt="monitor" />
+					</div>
+					<div id="printer">
+						<img src="/static/images/printer.svg" alt="printer" />
+					</div>
+					<div id="folder-bottom" />
+					<div id="text-bottom">
+						<h3>We'd love to meet you face to face.</h3>
+						<p />
+					</div>
+					<div id="bottom-break" />
 				</TeamContainer>
 			</AboutPage>
 		);
--- components/styles/AboutStyles.js
+++ components/styles/AboutStyles.js
@@ -1,31 +1,37 @@
 import styled from 'styled-components';
 
-const AboutPage = styled.div`
+const AboutPage = styled.main`
 	background: white;
-	display: grid;
-	grid-template-columns: repeat(12, minmax(100px, 1fr));
-	grid-template-rows: repeat(12, minmax(100px, 1fr));
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-flow: column nowrap;
 	padding: 0;
-	margin: 90px 0 0 0;
+	margin: 80px auto;
 	width: 100%;
 	height: 100%;
 `;
 
 const PageHeader = styled.div`
-	display: grid;
+	display: flex;
+	align-items: flex-start;
+	justify-content: center;
+	flex-flow: column nowrap;
 	background: transparent url(static/images/about-pattern-background.svg);
-	background-position-x: -120px;
-	background-position-y: -240px;
-	background-size: 110%;
+	background-position-x: left;
+	background-position-y: center;
+	background-size: 1920px;
 	background-repeat: no-repeat;
-	max-width: ${props => props.theme.maxWidth};
 	margin: 0 0 0 ${props => props.theme.marginLeft};
 	position: relative;
+	max-height: 450px;
+	padding-top: 8rem;
+	padding-bottom: 4rem;
 
-	grid-template-columns: minmax(calc(100vw - 70vw), 0.7fr);
-	grid-template-rows: [row1-start] 0.5fr [row1-end row2-start] 0.5fr [row2-end];
-	grid-column: span 12;
-	grid-row: 1 / 7;
+	.page-intro,
+	.team-greeting {
+		max-width: ${props => props.theme.maxWidth};
+	}
 
 	.page-intro {
 		display: flex;
@@ -35,7 +41,7 @@ const PageHeader = styled.div`
 		justify-self: flex-start;
 
 		h1 {
-			font-size: 5rem;
+			font-size: calc(64px + (92 - 64) * ((100vw - 1000px) / (1800 - 1000)));
 			line-height: 120%;
 			color: ${props => props.theme.grey};
 			font-family: ${props => props.theme.boldFont};
@@ -43,19 +49,20 @@ const PageHeader = styled.div`
 			margin: 0;
 		}
 		p {
-			font-size: 20px;
-			line-height: 100%;
+			font-size: calc(22px + (28 - 22) * ((100vw - 1000px) / (1800 - 1000)));
+			line-height: 120%;
 			color: ${props => props.theme.grey};
-			padding: 0;
+			padding: 1rem 0 0 0;
 			margin: 0;
+			width: 80%;
 		}
 	}
 
 	.team-greeting {
 		color: ${props => props.theme.yellow};
-		font-size: 40px;
-		margin: 0;
-		padding: 0;
+		font-size: calc(42px + (68 - 42) * ((100vw - 1000px) / (1800 - 1000)));
+		margin: 50px 0 0;
+		padding: 50px 0;
 		font-family: ${props => props.theme.boldFont};
 		line-height: 100%;
 	}
@@ -63,33 +70,181 @@ const PageHeader = styled.div`
 
 const TeamContainer = styled.div`
 	display: grid;
-	grid-template-columns: repeat(4, minmax(25%, 1fr));
-	grid-template-rows: repeat(4, minmax(25%, 1fr));
-	grid-column: span 12;
-	grid-row: span 6;
+	grid-template-columns: repeat(10, minmax(50px, 1fr));
+	grid-template-rows: 50px repeat(23, minmax(auto, 100px));
 	position: relative;
-	background-color: ${props => props.theme.grey};
 	height: 100%;
-`;
+	width: 100%;
+	margin-top: 50px;
 
-const MemberRow = styled.div`
-	display: grid;
-	grid: repeat(4, minmax(50px, 1fr)) / repeat(auto-fit, minmax(50px, 1fr));
-	grid-column: span 4;
-	grid-row: span 4;
-`;
+	.member-row {
+		display: grid;
+		grid-template-rows: 50px repeat(6, minmax(150px, 1fr));
+		grid-template-columns: repeat(24, minmax(auto, 1fr));
+		grid-column: 1 / 24;
+		grid-row: 2 / 9;
+		grid-auto-flow: dense;
+		grid-auto-rows: 50px;
+		grid-auto-columns: 50px;
+		width: 100%;
+		justify-items: center;
 
-const Member = styled.div`
-	display: grid;
-	grid: repeat(8, minmax(50px, 1fr)) / repeat(auto-fit, minmax(50px, 1fr));
-	grid-column: span 3;
-	grid-row: 2 / 3;
-	grid-auto-flow: row;
+		&#top {
+			.member {
+				display: grid;
+				grid: repeat(12, 50px) / repeat(12, 50px);
+				grid-row: 2 / 7;
+				grid-auto-flow: dense;
+				grid-auto-rows: 50px;
+				grid-auto-columns: 50px;
+				width: 100%;
+				max-width: 600px;
+				justify-items: center;
 
-	:first-child {
-		background-color: rgba(255, 124, 0, 0.5);
+				div > img {
+					height: auto;
+					width: 100%;
+					object-fit: contain;
+				}
+
+				.folder,
+				.person,
+				.bio {
+					width: 100%;
+				}
+
+				&#first {
+					grid-column: 1 / 13;
+					margin-top: 33px;
+					.folder {
+						z-index: 1000;
+						grid-row: 1 / 13;
+						grid-column: 2 / 13;
+						img {
+							max-width: 500px;
+						}
+					}
+					.person {
+						z-index: 5000;
+						grid-row: 1 / 6;
+						grid-column: 3 / 9;
+						transform: matrix(0.9781, -0.2079, 0.2079, 0.9781, -26, 35);
+						img {
+							max-width: 300px;
+						}
+					}
+					.bio {
+						z-index: 3000;
+						grid-row: 7 / 14;
+						grid-column: 6 / 11;
+						transform: matrix(0.9781, 0.2079, -0.2079, 0.9781, 37, -21);
+						img {
+							max-width: 285px;
+						}
+					}
+				}
+
+				&#second {
+					margin-top: -33px;
+					grid-column: 12 / 24;
+
+					.folder {
+						grid-row: 1 / 13;
+						grid-column: 2 / 13;
+						z-index: 1000;
+						img {
+							max-width: 500px;
+						}
+					}
+					.person {
+						transform: matrix(0.9903, 0.1392, -0.1392, 0.9903, 20, -19);
+						z-index: 3000;
+						grid-row: 3 / 5;
+						grid-column: 6 / 12;
+						img {
+							max-width: 350px;
+						}
+					}
+					.bio {
+						z-index: 5000;
+						transform: matrix(0.9903, -0.1392, 0.1392, 0.9903, -20, 18);
+						grid-row: 7 / 14;
+						grid-column: 5 / 10;
+						img {
+							max-width: 285px;
+						}
+					}
+				}
+			}
+		}
 	}
-	background-color: rgba(0, 124, 255, 0.5);
+
+	#grey-bg {
+		grid-column: 1 / -1;
+		grid-row: 3 / 10;
+		background-color: ${props => props.theme.grey};
+	}
+
+	#file-cabinet {
+		grid-column: 4 / 8;
+		grid-row: 7 / 14;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		z-index: 100;
+		/* width: 100%; */
+		img {
+			height: auto;
+			width: 100%;
+			max-width: 375px;
+		}
+	}
+
+	#monitor {
+		grid-column: 1 / span 5;
+		grid-row: 11 / span 5;
+		display: flex;
+		align-items: baseline;
+		justify-content: center;
+		z-index: 1000;
+		img {
+			height: auto;
+			width: 100%;
+			max-width: 600px;
+		}
+	}
+
+	#printer {
+		grid-column: 7 / 11;
+		grid-row: 12 / span 6;
+		display: flex;
+		align-items: baseline;
+		justify-content: center;
+		z-index: 1000;
+		img {
+			height: auto;
+			width: 100%;
+			max-width: 600px;
+		}
+	}
+
+	#folder-bottom,
+	#text-bottom {
+		display: none;
+	}
+
+	#bottom-break {
+		grid-column: 1 / -1;
+		grid-row: 14 / 18;
+		margin: 0px 0 15px 0;
+		background-color: ${props => props.theme.yellow};
+		background-image: url(/static/images/about-row-background.svg);
+		background-size: 50%;
+		background-position: center;
+		background-repeat: no-repeat;
+		display: flex;
+		width: 100% !important;
+	}
 `;
 
-export { AboutPage, PageHeader, TeamContainer, MemberRow, Member };
+export { AboutPage, PageHeader, TeamContainer };
