f2fee3c58f0b6b935f11ae05b368aba7f57361ff
Brock Boren <brock.boren@gmail.com>
2019-04-09 14:54:01 -0500

    Make nav menu collapse after item is clicked.

--- components/Nav.js
+++ components/Nav.js
@@ -1,12 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Link from 'next/link';
 import { NavBar, ItemStyles } from './styles/NavStyles';
 
 class Nav extends Component {
+	state = {
+		isChecked: false,
+	};
+
+	handleToggle = () => {
+		this.setState(prevState => ({
+			isChecked: !prevState.isChecked,
+		}));
+	};
+
 	render() {
 		return (
 			<NavBar className="nav__bar">
-				<input type="checkbox" id="toggle" aria-hidden="true" />
+				<input
+					type="checkbox"
+					id="toggle"
+					aria-hidden="true"
+					checked={this.state.isChecked}
+					onChange={this.handleToggle}
+				/>
 				<label htmlFor="toggle" className="nav__icon" aria-hidden="true">
 					Expand the Menu
 					<span className="nav__icon-line" />
@@ -16,27 +32,27 @@ class Nav extends Component {
 
 				<ItemStyles role="navigation" className="nav">
 					<ul className="nav__items">
-						<li className="nav__item">
+						<li className="nav__item" onClick={this.handleToggle}>
 							<Link prefetch href="/about-us">
 								<a>about us</a>
 							</Link>
 						</li>
-						<li className="nav__item">
+						<li className="nav__item" onClick={this.handleToggle}>
 							<Link prefetch href="/services">
 								<a>services</a>
 							</Link>
 						</li>
-						<li className="nav__item">
+						<li className="nav__item" onClick={this.handleToggle}>
 							<Link prefetch href="/contact">
 								<a>contact</a>
 							</Link>
 						</li>
-						<li className="nav__item">
+						<li className="nav__item" onClick={this.handleToggle}>
 							<Link prefetch href="/weekly-training">
 								<a>training</a>
 							</Link>
 						</li>
-						<li className="nav__item">
+						<li className="nav__item" onClick={this.handleToggle}>
 							<Link href="https://erm.yellowfolder.com/login">
 								<a target="_blank" rel="noopener noreferrer">
 									log in
