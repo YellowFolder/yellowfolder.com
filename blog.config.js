2a743cc84ce79d528bb836ed6dcbdc98800f3c9d
Brock Boren <brock.boren@gmail.com>
2019-10-31 14:59:52 -0500

    Create blog config.

---
+++ blog.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	siteMeta: {
+		title: 'YellowFolder',
+		author: 'YellowFolder',
+		image: '/static/images/yellowfolder.png',
+		description:
+			'YellowFolder provides a cloud-based, digital file cabinet where your school system can safely store, retrieve and manage all of their Administrative, Employee, Special Education, and Student Records.',
+		siteUrl: 'https://www.yellowfolder.com',
+		social: {
+			twitter: 'yellow_folder',
+			linkedin: 'https://www.linkedin.com/company/yellowfolder',
+			facebook: 'https://www.facebook.com/yellowfolder',
+		},
+		postsPerPage: 10,
+	},
+};
