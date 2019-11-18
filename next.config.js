24d0069570f6987aaa4e864a894016d8aef9070f
Brock Boren <brock.boren@gmail.com>
2019-11-18 15:02:17 -0600

    Remove CSS modules.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 // const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
 const withCSS = require('@zeit/next-css');
 
-module.exports = withCSS({});
+module.exports = withCSS({
+	cssModules: false,
+});
 
 // module.exports = withBundleAnalyzer({
 // 	enabled: process.env.ANALYZE === 'true',
