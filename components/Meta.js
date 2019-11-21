df49521894104c0a1bfcb171206f4a0af891cd4a
Brock Boren <brock.boren@gmail.com>
2019-11-20 19:08:05 -0600

    Remove [DEV] from document title.

--- components/Meta.js
+++ components/Meta.js
@@ -5,16 +5,37 @@ const Meta = () => (
 	<Head>
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<meta charSet="utf-8" />
-		<link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-touch-icon.png?v=kP3eE9yM02" />
-		<link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png?v=kP3eE9yM02" />
-		<link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png?v=kP3eE9yM02" />
+		<link
+			rel="apple-touch-icon"
+			sizes="180x180"
+			href="/static/favicon/apple-touch-icon.png?v=kP3eE9yM02"
+		/>
+		<link
+			rel="icon"
+			type="image/png"
+			sizes="32x32"
+			href="/static/favicon/favicon-32x32.png?v=kP3eE9yM02"
+		/>
+		<link
+			rel="icon"
+			type="image/png"
+			sizes="16x16"
+			href="/static/favicon/favicon-16x16.png?v=kP3eE9yM02"
+		/>
 		<link rel="manifest" href="/static/favicon/site.webmanifest?v=kP3eE9yM02" />
-		<link rel="mask-icon" href="/static/favicon/safari-pinned-tab.svg?v=kP3eE9yM02" color="#ffb520" />
+		<link
+			rel="mask-icon"
+			href="/static/favicon/safari-pinned-tab.svg?v=kP3eE9yM02"
+			color="#ffb520"
+		/>
 		<link rel="shortcut icon" href="/static/favicon/favicon.ico?v=kP3eE9yM02" />
 		<meta name="msapplication-TileColor" content="#2d89ef" />
 		<meta name="theme-color" content="#ffffff" />
-		<meta name="description" content="YellowFolder provides a cloud-based, digital file cabinet where your school system can safely store, retrieve and manage all of their Administrative, Employee, Special Education, and Student Records." />
-		<title> YellowFolder - [DEV]</title>{' '}
+		<meta
+			name="description"
+			content="YellowFolder provides a cloud-based, digital file cabinet where your school system can safely store, retrieve and manage all of their Administrative, Employee, Special Education, and Student Records."
+		/>
+		<title> YellowFolder</title>{' '}
 	</Head>
 );
 
