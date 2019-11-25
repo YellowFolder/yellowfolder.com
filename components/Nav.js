3a7a2047338f3b668771241abe00eb8306e20ae7
Brock Boren <brock.boren@gmail.com>
2019-11-25 11:00:11 -0600

    Prefetch links in Nav bar on hover.

--- components/Nav.js
+++ components/Nav.js
@@ -1,72 +1,63 @@
 import Link from 'next/link';
-import React, { Component } from 'react';
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
 import { ItemStyles, NavBar } from './styles/NavStyles';
 
-class Nav extends Component {
-	state = {
-		isChecked: false,
-	};
+const Nav = () => {
+	const router = useRouter();
+	const [isOpen, toggleNav] = useState(false);
 
-	handleToggle = () => {
-		console.log('handle toggle!');
-		this.setState(prevState => ({
-			isChecked: !prevState.isChecked,
-		}));
-	};
+	return (
+		<NavBar className="nav__bar">
+			<input
+				type="checkbox"
+				id="toggle"
+				aria-hidden="true"
+				checked={isOpen}
+				onChange={() => toggleNav(!isOpen)}
+			/>
+			<label htmlFor="toggle" className="nav__icon" aria-hidden="true">
+				Expand the Menu
+				<span className="nav__icon-line" />
+				<span className="nav__icon-line" />
+				<span className="nav__icon-line" />
+			</label>
 
-	render() {
-		return (
-			<NavBar className="nav__bar">
-				<input
-					type="checkbox"
-					id="toggle"
-					aria-hidden="true"
-					checked={this.state.isChecked}
-					onChange={this.handleToggle}
-				/>
-				<label htmlFor="toggle" className="nav__icon" aria-hidden="true">
-					Expand the Menu
-					<span className="nav__icon-line" />
-					<span className="nav__icon-line" />
-					<span className="nav__icon-line" />
-				</label>
-
-				<ItemStyles role="navigation" className="nav">
-					<ul className="nav__items">
-						<li className="nav__item" onClick={this.handleToggle}>
-							<Link href="/about-us">
-								<a>about us</a>
-							</Link>
-						</li>
-						<li className="nav__item" onClick={this.handleToggle}>
-							<Link href="/services">
-								<a>services</a>
-							</Link>
-						</li>
-						<li className="nav__item" onClick={this.handleToggle}>
-							<Link href="/contact">
-								<a>contact</a>
-							</Link>
-						</li>
-						<li className="nav__item" onClick={this.handleToggle}>
-							<Link href="/weekly-training">
-								<a>training</a>
-							</Link>
-						</li>
-						<li className="nav__item" onClick={this.handleToggle}>
-							<a
-								href="https://erm.yellowfolder.com/login/#"
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								log in
-							</a>
-						</li>
-					</ul>
-				</ItemStyles>
-			</NavBar>
-		);
-	}
-}
+			<ItemStyles role="navigation" className="nav">
+				<ul className="nav__items">
+					<li className="nav__item" onClick={() => toggleNav(!isOpen)}>
+						<Link href="/about-us">
+							<a onMouseEnter={() => router.prefetch('/about-us')}>about us</a>
+						</Link>
+					</li>
+					<li className="nav__item" onClick={() => toggleNav(!isOpen)}>
+						<Link href="/services">
+							<a onMouseEnter={() => router.prefetch('/services')}>services</a>
+						</Link>
+					</li>
+					<li className="nav__item" onClick={() => toggleNav(!isOpen)}>
+						<Link href="/contact">
+							<a onMouseEnter={() => router.prefetch('/contact')}>contact</a>
+						</Link>
+					</li>
+					<li className="nav__item" onClick={() => toggleNav(!isOpen)}>
+						<Link href="/weekly-training">
+							<a onMouseEnter={() => router.prefetch('/weekly-training')}>training</a>
+						</Link>
+					</li>
+					<li className="nav__item" onClick={() => toggleNav(!isOpen)}>
+						<a
+							href="https://erm.yellowfolder.com/login/#"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							log in
+						</a>
+					</li>
+				</ul>
+			</ItemStyles>
+		</NavBar>
+	);
+};
 
 export default Nav;
