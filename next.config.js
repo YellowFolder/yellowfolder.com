f47ab31382b9c94f5e37a56a5c89b9e47dfbf9ea
Brock Boren <brock.boren@gmail.com>
2019-11-25 15:54:52 -0600

    Fix issue where server-rendered routes would 404.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,25 @@
+const withCss = require('@zeit/next-css');
 const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
 
-module.exports = withBundleAnalyzer({
-	enabled: process.env.ANALYZE === 'true',
+module.exports = withCss({
 	target: 'serverless',
 	exportTrailingSlash: false,
+	exportPathMap: async function() {
+		const paths = {
+			'/': { page: '/' },
+			'/index.html': { page: '/' },
+			'/about-us.html': { page: '/about-us' },
+			'/contact.html': { page: '/contact' },
+			'/privacy-policy.html': { page: '/privacy-policy' },
+			'/record-request.html': { page: '/record-request' },
+			'/request-success.html': { page: '/request-success' },
+			'/schedule-a-yellow-folder-demo.html': { page: '/schedule-a-yellow-folder-demo' },
+			'/services.html': { page: '/services' },
+			'/weekly-training.html': { page: '/weekly-training' },
+		};
+
+		return paths;
+	},
 	webpack: config => {
 		// Fixes npm packages that depend on `fs` module
 		config.node = {
