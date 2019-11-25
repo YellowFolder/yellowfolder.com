bfccbf38a62e03c9f8b51693cb43ca33f9616925
Brock Boren <brock.boren@gmail.com>
2019-11-25 15:11:56 -0600

    Remove trailing slash from client side routes.

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
 module.exports = withBundleAnalyzer({
 	enabled: process.env.ANALYZE === 'true',
 	target: 'serverless',
-	exportTrailingSlash: true,
+	exportTrailingSlash: false,
 	webpack: config => {
 		// Fixes npm packages that depend on `fs` module
 		config.node = {
