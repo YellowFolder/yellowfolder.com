edc841291ce112345ccff615c71d9640d6f2db4e
Brock Boren <brock.boren@gmail.com>
2020-05-25 16:58:39 -0500

    Update packages.

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default class MyDocument extends Document {
 	// load styles before initial render so that page does not initially render without any styling.
 	// in other words, this prevents a flicker of the screen after render.
 	// unique to Next.js (https://github.com/zeit/next.js/#custom-document)
-	static getInitialProps({ renderPage }) {
+	static async getInitialProps({ renderPage }) {
 		const sheet = new ServerStyleSheet();
 		const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
 		const styleTags = sheet.getStyleElement();
