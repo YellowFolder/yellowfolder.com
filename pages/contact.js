0576e3d55185d9037df39a28d581c74797d1c714
Brock Boren <brock.boren@gmail.com>
2020-05-26 16:43:13 -0500

    Create record request tickets through freshdesk API.

--- pages/contact.js
+++ pages/contact.js
@@ -2,26 +2,31 @@ import React from 'react';
 import unirest from 'unirest';
 import Contact from '../components/Contact';
 
-const FRESHDESK_BASE_URL = 'https://brockboren.freshdesk.com';
-const FRESHDESK_PATH = `/api/v2/tickets/`;
-const FRESHDESK_KEY = process.env.FRESHDESK_KEY;
-
 export async function getStaticProps() {
-	let url = `${FRESHDESK_BASE_URL}${FRESHDESK_PATH}`;
+	const FRESHDESK_KEY = process.env.FRESHDESK_KEY_TEST;
+	const FRESHDESK_BASE_URL = process.env.FRESHDESK_BASE_URL;
+	let url = `${FRESHDESK_BASE_URL}/api/v2/tickets`;
+
 	let resp = await unirest.get(url).auth({
 		user: FRESHDESK_KEY,
 		sendImmediately: true,
 	});
+
 	let ticketList = resp.body;
 	let tickets = {};
+
 	await ticketList.map(ticket => {
 		let ticketId = ticket.id;
 		return (tickets[ticket.id] = { ...ticket });
 	});
+
 	console.log(tickets);
+
 	return {
 		props: {
-			...tickets,
+			tickets: {
+				...tickets,
+			},
 		},
 	};
 }
