914722c9037474437a5c9159b38eadd1fddf3f6f
Brock Boren <brock.boren@gmail.com>
2020-05-28 15:06:10 -0500

    Add student privacy pledge banner to privacy policy page.

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import unirest from 'unirest';
 import Contact from '../components/Contact';
 
 export async function getStaticProps() {
@@ -7,26 +6,9 @@ export async function getStaticProps() {
 	const FRESHDESK_BASE_URL = process.env.FRESHDESK_BASE_URL;
 	let url = `${FRESHDESK_BASE_URL}/api/v2/tickets`;
 
-	let resp = await unirest.get(url).auth({
-		user: FRESHDESK_KEY,
-		sendImmediately: true,
-	});
-
-	let ticketList = resp.body;
-	let tickets = {};
-
-	await ticketList.map(ticket => {
-		let ticketId = ticket.id;
-		return (tickets[ticket.id] = { ...ticket });
-	});
-
-	console.log(tickets);
-
 	return {
 		props: {
-			tickets: {
-				...tickets,
-			},
+			tickets: {},
 		},
 	};
 }
