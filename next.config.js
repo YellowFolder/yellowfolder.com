81c5f9e83accf408de37f1eeeb507ec9a186020a
Brock Boren <brock.boren@gmail.com>
2020-12-16 13:51:41 -0600

    Update env and add logo variants.

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ module.exports = withCss({
 		FRESHDESK_KEY: process.env.NEXT_PUBLIC_FRESHDESK_KEY_PROD,
 		FRESHDESK_BASE_URL: process.env.NEXT_PUBLIC_FRESHDESK_BASE_URL,
 		FRESHCHAT_TOKEN: process.env.NEXT_PUBLIC_FRESHCHAT_TOKEN,
+		WEBSITE_DOMAIN: process.env.WEBSITE_DOMAIN,
 	},
 	webpack: (config, { isServer }) => {
 		if (isServer) {
